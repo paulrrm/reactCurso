b44138caae93b8985e1187c4b6fb026186d24da0
paulruizm <pruiz@asiste.com.ec>
2024-05-30 17:04:07 -0500

    avasnce

--- 02_tiendaGuitarras/GuitarLA/src/App.jsx
+++ 02_tiendaGuitarras/GuitarLA/src/App.jsx
@@ -1,73 +1,14 @@
 
-import { useEffect, useState } from 'react'
 import './App.css'
 import Guitar from './components/Guitar'
 import Header from './components/Header'
-import { db } from './data/db'
+
 import useCart from './hooks/useCart'
 
 const App = () => {
 
-  const {auth} =useCart()
-  const initialCart =() =>{
-    const localStorageItem = localStorage.getItem('cardGuitarLa')
-    return localStorageItem ? JSON.parse(localStorageItem): []
-  }
-  const [data, setdata] = useState([])
-  const [card, setcard] = useState(initialCart)
-  function addToCard(item){
-    console.log("Agregando")
-    const itemExist = card.findIndex((guitar)=> guitar.id === item.id )
-    console.log(itemExist)
-    if(itemExist > -1){
-      console.log("YA existe")
-      const updatedCard = [...card]
-      updatedCard[itemExist].cantidad++
-      setcard(updatedCard)
-    }
-    else{
-      item.cantidad = 1
-      setcard(prevCard => [...prevCard, item])
-    }
-    
-  }
-  function removeToCart(id){
-    console.log('eliminando')
-    
-    const updatedCard = [...card]
-    const findIndex = updatedCard.findIndex(item => item.id === id)
-    updatedCard.splice(findIndex,1)
-    setcard(updatedCard)
-     
-  }
-  function variarCantidad(id , cantidad){
-    const updatedCard = [...card]
-    const findIndex = updatedCard.findIndex(item => item.id === id)
-    const resultadoVariarCantidad = updatedCard[findIndex].cantidad + cantidad
-    if(resultadoVariarCantidad > 0){
-      updatedCard[findIndex].cantidad = resultadoVariarCantidad
-      setcard(updatedCard)
-    }
-    else{
-        removeToCart(id)
-    }
-  }
-  function vaciarCarrito(){
-    setcard([])
-  }
-  function saveLocalStorage(){
-    localStorage.setItem('cardGuitarLa', JSON.stringify(card))
-  }
-  useEffect(() => {
-    setdata(db)
-  }, [])
-
-  useEffect(() => {
-   saveLocalStorage()
-  }, [card])
+  const {card, setcard, removeToCart,vaciarCarrito,variarCantidad,data,addToCard} = useCart();
   
-
-  console.log(data)
   return (
     <>
       <Header 
--- 02_tiendaGuitarras/GuitarLA/src/hooks/useCart.js
+++ 02_tiendaGuitarras/GuitarLA/src/hooks/useCart.js
@@ -1,11 +1,78 @@
+import { useEffect, useState } from "react"
 
 
 const useCart = () => {
-    console.log('desde usecard')
-    const auth = true
+    
+    const initialCart =() =>{
+      const localStorageItem = localStorage.getItem('cardGuitarLa')
+      return localStorageItem ? JSON.parse(localStorageItem): []
+    }
+    const [data, setdata] = useState([])
+    const [card, setcard] = useState(initialCart)
+    
+    function addToCard(item){
+      console.log("Agregando")
+      const itemExist = card.findIndex((guitar)=> guitar.id === item.id )
+      console.log(itemExist)
+      if(itemExist > -1){
+        console.log("YA existe")
+        const updatedCard = [...card]
+        updatedCard[itemExist].cantidad++
+        setcard(updatedCard)
+      }
+      else{
+        item.cantidad = 1
+        setcard(prevCard => [...prevCard, item])
+      }
+      
+    }
+    function removeToCart(id){
+      console.log('eliminando')
+      
+      const updatedCard = [...card]
+      const findIndex = updatedCard.findIndex(item => item.id === id)
+      updatedCard.splice(findIndex,1)
+      setcard(updatedCard)
+       
+    }
+    function variarCantidad(id , cantidad){
+      const updatedCard = [...card]
+      const findIndex = updatedCard.findIndex(item => item.id === id)
+      const resultadoVariarCantidad = updatedCard[findIndex].cantidad + cantidad
+      if(resultadoVariarCantidad > 0){
+        updatedCard[findIndex].cantidad = resultadoVariarCantidad
+        setcard(updatedCard)
+      }
+      else{
+          removeToCart(id)
+      }
+    }
+    function vaciarCarrito(){
+      setcard([])
+    }
+    function saveLocalStorage(){
+      localStorage.setItem('cardGuitarLa', JSON.stringify(card))
+    }
+    useEffect(() => {
+      setdata(db)
+    }, [])
+  
+    useEffect(() => {
+     saveLocalStorage()
+    }, [card])
+    
+  
+    console.log(data)
 
     return{
-        auth,
+        vaciarCarrito,
+        saveLocalStorage,
+        variarCantidad,
+        removeToCart,
+        addToCard,
+        card,
+        setcard,
+        data
     }
 
 }
