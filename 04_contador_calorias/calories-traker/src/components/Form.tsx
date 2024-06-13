65524a5cfa84ad72eb249670e8127d0a0e1e8523
paulrrm <paulruizm@gmail.com>
2024-06-12 22:52:39 -0700

    avance

--- 04_contador_calorias/calories-traker/src/components/Form.tsx
+++ 04_contador_calorias/calories-traker/src/components/Form.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
+import { Activity } from "../types"
+
 const Form = () => {
+    const [activity, setActivity] = useState<Activity>({
+        category:1,
+        name:'',
+        calories: 0
+
+    })
+    const handleChange =(e: React.ChangeEvent<HTMLSelectElement>|React.ChangeEvent<HTMLInputElement>)=>{
+        const isNumberField = ['category','calories'].includes(e.target.id)
+        setActivity({
+            ...activity,
+            [e.target.id]:isNumberField ? +e.target.value: e.target.value
+        })
+        
+    }
     return (
         <form
             className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -9,6 +26,8 @@ const Form = () => {
                 <select
                     className="border border-slate-200 p-2 rounded-lg w-full bg-white"
                     id="category"
+                    value={activity.category}
+                    onChange={handleChange}
                 >
                     {categories.map(category => (
                         <option
@@ -22,12 +41,14 @@ const Form = () => {
                 </select>
             </div>
             <div className="grid grid-cols-1 gap-3">
-                <label htmlFor="activity" className="font-bold">Actividad:</label>
+                <label htmlFor="name" className="font-bold">Actividad:</label>
                 <input 
-                id="activity"
+                id="name"
                 type="text"
                 className="border border-slate-200 p-2 rounded-lg"
                 placeholder="Ej. Comida, jugo de naranjo, Ejercico, caminata , etc"
+                value={activity.name}
+                onChange={handleChange}
                 >
                 </input>
             </div>
@@ -38,6 +59,8 @@ const Form = () => {
                 type="number"
                 className="border border-slate-200 p-2 rounded-lg"
                 placeholder="Ej. 200 , 400 ,800"
+                value={activity.calories}
+                onChange={handleChange}
                 >
                 </input>
             </div>
